98f2ddd3f82b6019741e12ed1b6c1054bd6665ad
Christopher J. Rogers <crogers3@mit.edu>
2014-04-19 12:13:22 -0400

    Added unregister server request

--- ReuseMobile/src/com/example/reusemobile/views/UnregisterDialog.java
+++ ReuseMobile/src/com/example/reusemobile/views/UnregisterDialog.java
@@ -1,11 +1,21 @@
 package com.example.reusemobile.views;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.preference.DialogPreference;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
+import android.widget.Toast;
+
+import com.example.reusemobile.GlobalApplication;
+import com.example.reusemobile.logging.Sting;
 
 public class UnregisterDialog extends DialogPreference {
     Context context;
@@ -19,6 +29,7 @@ public class UnregisterDialog extends DialogPreference {
     protected void onDialogClosed(boolean positiveResult) {
         if(positiveResult) {
             // Unregister User
+            new SendUnregisterRequest().execute();
             SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
             pref.edit().remove("username").commit();
             pref.edit().putBoolean("isVerified", false).commit();
@@ -29,6 +40,42 @@ public class UnregisterDialog extends DialogPreference {
         }
     }
     
-    
+    private class SendUnregisterRequest extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+         // Create a new HttpClient and Post Header
+            String port = GlobalApplication.serverPort;
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://armadillo.xvm.mit.edu:" + port + "/api/login/unregister/");
+            String email = PreferenceManager.getDefaultSharedPreferences(context).getString("username", "");
+            httppost.addHeader("USERNAME", email);
+            String token = PreferenceManager.getDefaultSharedPreferences(context).getString("token", "");
+            httppost.addHeader("TOKEN", token);
+            try {
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+                
+                if(response.getStatusLine().getStatusCode() != 200) {
+                    Sting.logError((Activity) context, Sting.CLAIM_ERROR, response.getStatusLine().getReasonPhrase());
+                    return "An error occured in unregister:\n" + response.getStatusLine().getReasonPhrase();
+                }
+                
+                return "Successfully Unregistered";
+            } catch (Exception e) {
+                Sting.logError((Activity) context, Sting.CLAIM_ERROR, "Exception: " + e.getLocalizedMessage());
+                return "An exception occured during unregister:\n" + e.getLocalizedMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+            if(result == null) {
+            } else {
+                if(GlobalApplication.debug) Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     
 }
