450f77581d777dd8c311991311a19cf5179e1a19
Christopher J. Rogers <crogers3@mit.edu>
2014-05-04 23:10:08 -0400

    Changed back button behavior for ItemDetails

--- ReuseMobile/src/com/example/reusemobile/ItemDetails.java
+++ ReuseMobile/src/com/example/reusemobile/ItemDetails.java
@@ -121,11 +121,14 @@ public class ItemDetails extends ActionBarActivity implements ConfirmClaim.Confi
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        switch(item.getItemId()) {
+        case R.id.action_settings:
             return true;
-        }
-        if (id == R.id.action_map_view) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+            finish();
+            return true;
+        case R.id.action_map_view:
             Intent intent = new Intent(this, MapView.class);
             intent.putExtra(MainStream.ITEM_ID, itemId);
             Sting.logButtonPush(this, Sting.ACTION_MAP);
